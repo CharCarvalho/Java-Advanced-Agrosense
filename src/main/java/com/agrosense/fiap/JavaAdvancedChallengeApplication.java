290835cfc04fe646a94729301297b46569bab4a0
CharCarvalho <charlescarvalho97@outlook.com>
2024-10-06 20:20:54 -0300

    Console removidos

--- src/main/java/com/agrosense/fiap/JavaAdvancedChallengeApplication.java
+++ src/main/java/com/agrosense/fiap/JavaAdvancedChallengeApplication.java
@@ -11,7 +11,6 @@ public class JavaAdvancedChallengeApplication {
 	public static void main(String[] args) {		
 		Dotenv dotenv = Dotenv.load();
         System.setProperty("SPRING_DATASOURCE_URL", dotenv.get("SPRING_DATASOURCE_URL"));
-        System.out.println(dotenv.get("SPRING_DATASOURCE_URL"));
         System.setProperty("SPRING_DATASOURCE_USERNAME", dotenv.get("SPRING_DATASOURCE_USERNAME"));
         System.setProperty("SPRING_DATASOURCE_PASSWORD", dotenv.get("SPRING_DATASOURCE_PASSWORD"));
 
