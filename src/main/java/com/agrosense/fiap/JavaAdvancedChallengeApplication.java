cb7b161e598d7fa568cccda7a7c7ea8d6f985ea5
antonio.agasantos05 <aagomes0505@gmail.com>
2024-10-08 17:48:57 -0300

    Arrumando para executar o cp

--- src/main/java/com/agrosense/fiap/JavaAdvancedChallengeApplication.java
+++ src/main/java/com/agrosense/fiap/JavaAdvancedChallengeApplication.java
@@ -3,18 +3,10 @@ package com.agrosense.fiap;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import io.github.cdimascio.dotenv.Dotenv;
-
 @SpringBootApplication
 public class JavaAdvancedChallengeApplication {
 
-	public static void main(String[] args) {		
-		Dotenv dotenv = Dotenv.load();
-        System.setProperty("SPRING_DATASOURCE_URL", dotenv.get("SPRING_DATASOURCE_URL"));
-        System.setProperty("SPRING_DATASOURCE_USERNAME", dotenv.get("SPRING_DATASOURCE_USERNAME"));
-        System.setProperty("SPRING_DATASOURCE_PASSWORD", dotenv.get("SPRING_DATASOURCE_PASSWORD"));
-
+    public static void main(String[] args) {
         SpringApplication.run(JavaAdvancedChallengeApplication.class, args);
-	}
-
+    }
 }
